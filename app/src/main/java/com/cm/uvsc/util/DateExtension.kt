1ec0e2e6ec2a76fe92e10fc0bb8baff61a563cb7
cmyh <ksemfn@gmail.com>
2025-08-02 23:29:53 +0900

    refactor(Ble) : LocalDate Extension 리팩토링

--- app/src/main/java/com/cm/uvsc/util/DateExtension.kt
+++ app/src/main/java/com/cm/uvsc/util/DateExtension.kt
@@ -1,13 +1,21 @@
 package com.cm.uvsc.util
 
+import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toJavaLocalDateTime
+import kotlinx.datetime.toLocalDateTime
+import java.time.format.DateTimeFormatter
+import java.util.Locale
 
-fun LocalDateTime.nowDateString(): String {
-    fun Int.pad() = this.toString().padStart(2, '0')
-    val month = this.monthNumber.pad()
-    val day = this.dayOfMonth.pad()
-    val hour = this.hour.pad()
-    val minute = this.minute.pad()
-    val second = this.second.pad()
-    return "${this.year}-$month-$day $hour:$minute:$second"
+
+fun LocalDateTime.Companion.now(timeZone: TimeZone = TimeZone.currentSystemDefault()): LocalDateTime {
+    return Clock.System.now().toLocalDateTime(timeZone)
+}
+
+
+fun LocalDateTime.asDateString(format: String = "yyyy-MM-dd HH:mm:ss"): String {
+    val javaLocalDateTime = this.toJavaLocalDateTime()
+    val formatter = DateTimeFormatter.ofPattern(format, Locale.getDefault())
+    return javaLocalDateTime.format(formatter)
 }
