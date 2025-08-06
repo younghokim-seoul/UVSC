5f1be24d82985a03dbe9f520790eab6bccfcb404
cmyh <ksemfn@gmail.com>
2025-08-07 00:54:17 +0900

    feat(permission): 다이얼로그 추가 및 권한체크 함수 보강

--- app/src/main/java/com/cm/geofence/utils/ActivityExt.kt
+++ app/src/main/java/com/cm/geofence/utils/ActivityExt.kt
@@ -3,8 +3,22 @@ package com.cm.geofence.utils
 import android.Manifest
 import android.app.Activity
 import android.content.pm.PackageManager
+import android.os.Build
 
 fun Activity.checkLocationPermission(): Boolean {
-    return checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
-            checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    val fineLocationGranted = checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    val coarseLocationGranted = checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        return (fineLocationGranted || coarseLocationGranted) && checkSelfPermission(Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+    return fineLocationGranted || coarseLocationGranted
+}
+
+fun Activity.checkNotificationPermission(): Boolean {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        checkSelfPermission(Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+    } else {
+        true
+    }
 }
