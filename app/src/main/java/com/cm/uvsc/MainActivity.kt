a1a0c14b1fd233a87e5cad3dc73b003018d3a0dc
hyuna <hyuna9787@golfzon.com>
2025-08-07 11:20:31 +0900

    모드 변경 UI 오류 수정

--- app/src/main/java/com/cm/uvsc/MainActivity.kt
+++ app/src/main/java/com/cm/uvsc/MainActivity.kt
@@ -106,8 +106,13 @@ class MainActivity : ComponentActivity() {
             viewModel.uiEvent.collect {
                 when (it) {
                     is UiEvent.ModeChanged -> {
-                        Toast.makeText(this@MainActivity, "모드 변경이 완료되었습니다", Toast.LENGTH_LONG)
-                            .show()
+                        if (it.isSuccess) {
+                            Toast.makeText(this@MainActivity, "모드 변경이 완료되었습니다", Toast.LENGTH_LONG)
+                                .show()
+                        } else {
+                            Toast.makeText(this@MainActivity, "모드 변경에 실패했습니다.", Toast.LENGTH_LONG)
+                                .show()
+                        }
                     }
 
                     is UiEvent.ConnectResult -> {
